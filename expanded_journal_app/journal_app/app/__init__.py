810da6ae96090ab00ca946148a65fa698157002b
j-byars <jbyars2@vols.utk.edu>
2025-05-05 16:39:08 -0500

    create_view_edit_journaling

---
+++ expanded_journal_app/journal_app/app/__init__.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, current_app
+from config import Config
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+from flask_bootstrap import Bootstrap5
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+login.login_view = 'auth.login'
+login.login_message = 'Log in required.'
+bootstrap = Bootstrap5()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    bootstrap.init_app(app)    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app) 
+
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    from app.journal import bp as journal_bp
+    app.register_blueprint(journal_bp, url_prefix='/journal')
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    if not app.debug and not app.testing:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/journal_app.log', maxBytes=10240,  backupCount=10)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Digital-Journal-App startup')
+
+    return app
+
+from app import models
---
+++ expanded_journal_app/journal_app/app/journal/forms.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timezone
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, EqualTo, Length
+from flask import render_template, flash, redirect, url_for, request, current_app
+from flask_login import current_user, login_required
+import sqlalchemy as sa
+from app import db
+from app.models import User, JournalEntry, JournalEntryTag, Tag
+from app.journal import bp
+
+class JournalEntryForm(FlaskForm):
+    title = StringField(('Title'), validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    save_draft = SubmitField('Save Draft')
+    submit = SubmitField('Save')
+
+class EditJournalEntryForm(FlaskForm):
+    title = StringField(('Title'), validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Save')

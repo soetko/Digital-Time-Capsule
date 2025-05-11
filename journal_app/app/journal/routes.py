9ae812a9fd2544e4fa91fb9c1de06075f0f85e6c
j-byars <jbyars2@vols.utk.edu>
2025-05-11 09:28:54 -0500

    revert_and_update

--- journal_app/app/journal/routes.py
+++ journal_app/app/journal/routes.py
@@ -27,14 +27,14 @@ from app.tags_generation.tags_generation import tags_generator
 @login_required
 def journal():
 
-
+    
     test_entry_exists()
     entries = JournalEntry.query.filter_by(user_id=current_user.user_id).order_by(JournalEntry.entry_published.desc()).all()
 
     query = sa.select(Tag)
     tags = db.session.scalars(query).all()
-    for x in tags:
-        flash(x)
+    #for x in tags:
+    #    flash(x)
 
 
     journal_entries = []
@@ -48,9 +48,9 @@ def journal():
             'date': pd.to_datetime(entry.entry_published).strftime('%B %d, %Y at %H:%M GMT'),
             'image_path': ['blank', entry.media_id][0 if entry.media_id is None else 1],
             'content': entry.entry_body,
-            'tags': tags
+            'tags': tags.copy()
         }
-
+        #flash(tags_generator(entry.entry_body))
         if journal_entry[the_key]['image_path'] != 'blank':
             media_file_info = db.first_or_404(sa.select(Media).where(Media.media_id == entry.media_id))
                     
@@ -81,7 +81,7 @@ def new_entry():
             user_id = current_user.user_id,            
             entry_title=form.title.data,
             media_id=None,
-            entry_body=form.content.data.replace('\n', '<br/>')
+            entry_body=form.content.data#.replace('\n', '<br/>')
         )
         file = request.files['media_file']
         if file.filename != '':
@@ -91,7 +91,8 @@ def new_entry():
             return redirect(url_for('journal.journal'))            
         
         db.session.add(new_entry)
-        db.session.flush() 
+        db.session.commit()
+        #db.session.flush() 
 
         tags_str = form.tags.data
         if tags_str:
@@ -102,9 +103,9 @@ def new_entry():
                     if not tag:
                         tag = Tag(tag_name=tag_name)
                         db.session.add(tag)
-                    new_entry.tags.append(tag)
-
-        db.session.commit()
+                        new_entry.tags.append(tag)
+                        db.session.commit()
+        
         return redirect(url_for('journal.journal'))    
     else:
         return render_template('journal/new_entry.html', form=form)
@@ -123,6 +124,7 @@ def generate_entry_tags():
     data = request.get_json()
     content = data.get('content', '')
     generated_tags = tags_generator(content)
+    #flash('hi')
     return json.dumps({'tags': generated_tags})
 
 
@@ -187,28 +189,32 @@ def edit_entry(entry_id=None, media_id=None):
         upd_entry = db.first_or_404(sa.select(JournalEntry)
             .where(JournalEntry.entry_id == entry_id and JournalEntry.user_id == current_user.user_id))
 
-        upd_entry.entry_title=form.title.data
-        upd_entry.entry_body=form.content.data
-        # Clear existing tags
-        upd_entry.tags = []
-        
-        # Process tags
-        new_tags_str = form.tags.data
-        new_tags = [tag.strip() for tag in new_tags_str.split(',') if tag.strip()]
 
+        upd_entry.entry_title=form.title.data
+        upd_entry.entry_body=form.content.data#.replace('\n', '<br/>')
         db.session.add(upd_entry)
         db.session.commit()
+          
+        # Process tags
 
-        # # Add the new tags
-        # for tag_name in new_tags:
-        #     tag = db.session.scalar(sa.select(Tag).where(Tag.tag_name == tag_name))
-        #     if not tag:
-        #         tag = Tag(tag_name=tag_name)
-        #         db.session.add(tag)
-        #     entry.tags.append(tag)
-
-
-
+        tags_str = form.tags.data
+        if tags_str:
+            new_tags = list()
+            tags_list = [tag.strip() for tag in tags_str.split(',')]
+            for tag_name in tags_list:
+                if tag_name:
+                    tag = db.session.scalar(sa.select(Tag).where(Tag.tag_name == tag_name))
+                    if not tag:
+                        tag = Tag(tag_name=tag_name)
+                        db.session.add(tag)
+                    new_tags.append(tag)
+            #flash(new_tags)           
+            upd_entry = db.first_or_404(sa.select(JournalEntry)
+                 .where(JournalEntry.entry_id == entry_id and JournalEntry.user_id == current_user.user_id))  
+            upd_entry.tags = []
+            for tag in new_tags:
+                upd_entry.tags.append(tag)
+            db.session.commit()
 
 
         if upd_entry.media_id is not None:
@@ -225,7 +231,7 @@ def edit_entry(entry_id=None, media_id=None):
                     new_file = upload_file(file, upd_entry, False)
                     if new_file is None:
                         flash("it's none")
-                        return render_template('journal/upd_entry.html', form=form)
+                        return render_template('journal/edit_entry.html', form=form)
                     else:
                         m_id = db.first_or_404(sa.select(Media).where(Media.media_id == upd_entry.media_id))
                         file_ext = os.path.splitext(new_file.split('/')[-1])[1]
@@ -243,8 +249,9 @@ def edit_entry(entry_id=None, media_id=None):
                 new_file = upload_file(file, upd_entry, False)
                 if new_file is None:
 
-                    return render_template('journal/upd_entry.html', form=form)
+                    return render_template('journal/edit_entry.html', form=form)
                 else:
+                    #flash(new_file)
                     new_media = Media(
                         media_file_path = new_file,
                         media_format = os.path.splitext(new_file.split('/')[-1])[1]
@@ -254,6 +261,7 @@ def edit_entry(entry_id=None, media_id=None):
                     upd_entry = db.first_or_404(sa.select(JournalEntry)
                         .where(JournalEntry.entry_id == entry_id and JournalEntry.user_id == current_user.user_id))
                     upd_entry.media_id = new_media.media_id # media_id back populates after commit
+                    #upd_entry.entry_body=form.content.data#.replace('\n', '<br/>')
                     db.session.add(upd_entry)
                     db.session.commit()
 
@@ -263,9 +271,10 @@ def edit_entry(entry_id=None, media_id=None):
             # TODO: Delete row? Replace with 1 pixel image?
             pass
         
-        if upd_entry is not None and m_id is None and media_id is None:
-            db.session.add(upd_entry)
-            db.session.commit()
+        # if upd_entry is not None and m_id is None and media_id is None:
+        #     upd_entry.entry_body=form.content.data#.replace('\n', '<br/>')
+        #     db.session.add(upd_entry)
+        #     db.session.commit()
             
         return redirect(url_for('journal.journal'))    
 
@@ -277,6 +286,7 @@ def edit_entry(entry_id=None, media_id=None):
                 .where(JournalEntry.entry_id == entry_id and JournalEntry.user_id == current_user.user_id))
             form.title.data = entry.entry_title
             form.content.data = entry.entry_body
+            form.tags.data = entry.tags
             media_id = entry.media_id
             form.tags.data = ', '.join([tag.tag_name for tag in entry.tags])
     else:
